366276302607ae9a8a9c60b5afa1af9252578119
FrancescoGhisi <1072325009@ulife.com.br>
2024-04-06 00:45:05 -0300

    first commit

---
+++ src/Beecrowd1177.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Beecrowd1177 {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int T = input.nextInt();
+        input.close();
+        int valor = 0;
+        int [] Numero = new int[9];
+
+        for(int i = 0; i < Numero.length; i++){
+            if (valor < T) {
+                Numero[i] = valor;
+                System.out.println("N[" + i + "] = " + Numero[i]);
+                valor++;
+            } else{
+                valor = 0;
+                Numero[i] = valor;
+                System.out.println("N[" + i + "] = " + Numero[i]);
+                valor++;
+            }
+        }
+    }
+}
